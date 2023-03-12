7d85c14c1040d9be7f58491e5d7618bd991daf0f
Bomin <42407430+jebomin@users.noreply.github.com>
2023-03-12 12:33:45 +0900

    Transfer data using 'props'

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'Car Insurance'
-  const expenseAmount = 294.67;
-
+function ExpenseItem(props) {
   return (
-    <div className="expense-item">
-      <div>{expenseDate.tolSOString}</div>
-      <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+    <div className='expense-item'>
+      <div>{props.date.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
